0386a0fe6723785e8d9797f01f23fc9a7fded8e0
César <paulo1988cesar@msn.com>
2019-11-11 21:44:43 -0300

    Config

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,8 +14,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unabled to find location!', undefined)
         } else {
-            callback(undefined, 'it is currently: ' + body.currently.temperature + " degree out. There is a " +
-                body.currently.precipProbability + " chance of rain")
+            callback(undefined, 'it is currently: ' + body.currently.temperature + " degree out. This high today is " + body.daily.data[0].temperatureHigh 
+            + " with a low of " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + " chance of rain")
 
         }
     })
